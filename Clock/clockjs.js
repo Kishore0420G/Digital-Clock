4b9faf13ed043144b01a2dba4133dc3c278c1167
kishore p <107325190+Kishore0420G@users.noreply.github.com>
2024-08-30 18:05:51 +0530

    Add files via upload

---
+++ Clock/clockjs.js
@@ -0,0 +1,11 @@
+let hr = document.getElementById("hr");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+setInterval(()=>{
+    let CurrentTime = new Date();
+    hr.innerHTML = (CurrentTime.getHours()<10?"0":"") + CurrentTime.getHours();
+    min.innerHTML = (CurrentTime.getMinutes()<10?"0":"") + CurrentTime.getMinutes();
+    sec.innerHTML = (CurrentTime.getSeconds()<10?"0":"") + CurrentTime.getSeconds();
+},1000)
+
